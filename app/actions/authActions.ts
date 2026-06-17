b521fdf6f20a9afd17edc82845926720bd007f5d
mohamadgad83 <mood20800@gmail.com>
2026-06-17 16:21:03 +0300

    Create authActions.ts

---
+++ app/actions/authActions.ts
@@ -0,0 +1,6 @@
+'use server'
+
+// ملف وهمي تم إنشاؤه لاجتياز خطأ الـ Webpack في Vercel بنجاح
+export async function LoginUser(identifier: any, password: any) {
+  return { success: false, error: "Deprecated Action" };
+}
